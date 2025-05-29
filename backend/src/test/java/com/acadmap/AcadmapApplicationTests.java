02d3bd01c600af40d858bd786b5ba8590be96b40
yurilsn <luizyuri57@gmail.com>
2025-05-28 21:00:03 -0300

    refactor: add spring project with the directory structure

---
+++ backend/src/test/java/com/acadmap/AcadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
