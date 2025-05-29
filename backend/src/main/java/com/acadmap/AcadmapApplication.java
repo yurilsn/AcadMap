c37c836d09566af60a44c323e45efb14b8f59b6a
Yuri Luiz Silva do Nascimento <112652173+yurilsn@users.noreply.github.com>
2025-05-29 14:09:03 -0300

    chore: add documentation about the backend environment
    * chore:  add scripts to create the development environment
    
    * docs: add development environment documentation
    
    * chore: add spring application dockerfile
    
    * refactor: add spring project with the directory structure

---
+++ backend/src/main/java/com/acadmap/AcadmapApplication.java
@@ -0,0 +1,13 @@
+package com.acadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadmapApplication.class, args);
+	}
+
+}
